aa2d9b14b8f5201ea86c90bfa85e6e01055232b1
Natalie Ruth <natalieruth@natalies-mbp.attlocal.net>
2021-08-05 20:49:13 -0700

    feat:notes app complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from 'react'
 
-function App() {
+
+const App = ()=>{
+  //for storing and updating the text in the text area
+  const [text,updateText] = useState(" ");
+  const [autoStatus, setAutoStatus] = useState(false);
+  
+  const [isSaved,setIsSaved] = useState(false);
+
+  const on_change = (event)=>
+  {
+   const newValue = event.target.value;
+   updateText(newValue);
+   setIsSaved(false);
+  }
+
+  useEffect(()=>{
+    updateText(localStorage.getItem("note"));
+    setIsSaved(true);
+  },[]);
+
+
+  useEffect(()=>{
+    if(autoStatus === true){
+    autoSave();
+    }
+  },[text, autoStatus]);
+  
+
+  function toggleAuto()
+  {
+    setAutoStatus(!autoStatus);
+  }
+  const saveText = () =>
+  {
+    localStorage.setItem("note", text);
+    setIsSaved(true);
+    
+  }
+
+  //automatically save the users typed text
+  const autoSave = () =>
+  {
+   localStorage.setItem("note",text);
+   setIsSaved(true);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className = "w-full h-screen flex flex-row justify-center" > 
+   <div className = "flex flex-col justify-center"> 
+     <textarea
+      value = {text}
+      onChange = {on_change}
+      className = "border border-black">
+    </textarea>
+     <button className = "border border-black bg-green-300" onClick = {saveText}>Save Text</button>
+     {isSaved? "Text Saved!":"Save if you wish!"}
+     <button className = "border border-black btn" onClick = {toggleAuto}>AutoSave?</button>
+     {autoStatus?"Auto is on":"Auto is off"}
+     </div>
+   </div>
   );
 }
 
